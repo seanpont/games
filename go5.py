663e1f793e20e47fffb4b9c5e61d6a66e486dad2
Sean Pont <seanpont@gmail.com>
2015-06-07 16:43:35 -0700

    networked go5

--- go5.py
+++ go5.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
 # coding=utf-8
 
 import string
+import socket
+
 
 def find_index_where(arr, condition):
     for i, a in enumerate(arr):
@@ -9,70 +12,163 @@ def find_index_where(arr, condition):
     return -1
 
 
-def in_a_row(param, turn):
-    pass
+class bcolors:
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    UNDERLINE = '\033[4m'
 
 
 class Game(object):
-    def __init__(self, goal, board_size):
+    def __init__(self, size, goal, p1_token, p2_token):
+        self.size = size
         self.goal = goal
-        self.size = board_size
-        self.board = [[' ']*board_size for _ in xrange(board_size)]
-        self.p1, self.p2 = ' ', ' '
+        self.board = [[' ']*size for _ in xrange(size)]
+        self.p1, self.p2 = p1_token, p2_token
+        self.p1 = bcolors.GREEN + self.p1 + bcolors.ENDC
+        self.p2 = bcolors.BLUE + self.p2 + bcolors.ENDC
         self.turn = self.p1
+        self.winner = None
 
-    def print_board(self):
-        print ''
-        print '     ' + '   '.join(string.ascii_lowercase[:self.size])
-        print '   ' + '—' * (self.size*4 + 1)
+    def __str__(self):
+        # noinspection PyListCreation
+        lines = ['']
+        lines.append('     ' + '   '.join(string.ascii_lowercase[:self.size]))
+        lines.append('   ' + '—' * (self.size*4 + 1))
         for i, row in enumerate(self.board):
-            print '%2d' % (i+1) + ' | ' + ' | '.join(row) + ' | ' + str(i+1)
-            print '   ' + '—' * (self.size*4 + 1)
-        print '     ' + '   '.join(string.ascii_lowercase[:self.size])
-        print ''
+            lines.append('%2d' % (i+1) + ' | ' + ' | '.join(row) + ' | ' + str(i+1))
+            lines.append('   ' + '—' * (self.size*4 + 1))
+        lines.append('     ' + '   '.join(string.ascii_lowercase[:self.size]))
+        lines.append('')
+        return '\n'.join(lines)
 
-    def play(self):
-        print 'Welcome to Go5! Get %d in a row to win!' % self.goal
-        print 'Player one: select your token'
-        self.p1 = raw_input("Player 1: ").strip()[0]
-        self.p2 = raw_input("Player 2: ").strip()[0]
-        self.turn = self.p1
-        print 'enter your move by typing the row and column, like this: 5e'
-        while True:
-            self.play_turn()
-
-    def play_turn(self):
-        self.print_board()
-        play = raw_input("%s : " % self.turn)
-        index = find_index_where(play, lambda char: char in string.lowercase)
+    def play_turn(self, move):
+        index = find_index_where(move, lambda char: char in string.lowercase)
         if index == -1:
             print 'please enter the row then the column, like this: 5d'
             return
-        row = int(play[:index]) - 1
-        col = string.ascii_lowercase.find(play[index:])
+        row = int(move[:index]) - 1
+        col = string.ascii_lowercase.find(move[index:])
         if self.board[row][col] == ' ':
             self.board[row][col] = self.turn
-            if self.winner(row, col):
-                print 'Player %s wins!' % self.turn
-                exit()
-            self.turn = self.p2 if self.turn == self.p1 else self.p1
+            if self.has_won(row, col):
+                self.winner = self.turn
+            else:
+                # flip turn
+                if self.turn == self.p1:
+                    self.turn = self.p2
+                else:
+                    self.turn = self.p1
         else:
             print "That spot is taken!"
 
-    def winner(self, row, col):
-        # check row
-        in_a_row(self.board[row], self.turn)
+    def test_for_goal(self, tokens):
+        """
+        :param tokens: a sequence of tokens
+        :return: true if there are goal sequences of turn in a row
+        """
+        in_a_row = 0
+        for token in tokens:
+            if token == self.turn:
+                in_a_row += 1
+            else:
+                in_a_row = 0
+            if in_a_row == self.goal:
+                return True
         return False
 
+    def has_won(self, row_index, col_index):
+        in_a_row = 0
+        # test row
+        row = self.board[row_index]
+        if self.test_for_goal(row):
+            return True
+        col = [row[col_index] for row in self.board]
+        if self.test_for_goal(col):
+            return True
+        return None
 
-game = Game(4, 9)
-game.play()
+
+def game_console():
+    size = raw_input("Board size: ")
+    goal = raw_input("How many in a row to win? ")
+    p1 = raw_input("Player 1 token: ").strip()[0]
+    p2 = raw_input("Player 2 token: ").strip()[0]
+
+    game = Game(goal, size, p1, p2)
+    print 'enter your move by typing the row and column, like this: 5e'
+    while not game.winner:
+        print game
+        move = raw_input("%s : " % game.turn)
+        game.play_turn(move)
+    print "Player %s wins!" % game.winner
 
 
+def game_host():
+    size = int(raw_input("Board size: "))
+    goal = int(raw_input("How many in a row to win? "))
+    p1 = raw_input("Player 1 token: ").strip()[0]
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (socket.gethostbyname(socket.gethostname()), 8000)
+    print 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
+    sock.listen(5)
+    print 'waiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        p2 = connection.recv(256).strip()
+        print 'p2 token: ', p2
+        game = Game(size, goal, p1, p2)
+        connection.sendall('%s,%s,%s,%s' % (size, goal, p1, p2))
+        while not game.winner:
+            print game
+            if game.turn == game.p1:
+                move = raw_input("%s : " % game.turn)
+                connection.sendall(move)
+            else:
+                move = connection.recv(64).strip()
+            game.play_turn(move)
+        print "Player %s wins!" % game.winner
+    finally:
+        connection.close()
 
 
+def game_client():
+    ip_addr = raw_input("Enter host ip: ").strip()
+    p2 = raw_input("Enter your token: ").strip()[0]
+    connection = socket.create_connection((ip_addr, 8000))
+    try:
+        connection.sendall(p2)
+        data = connection.recv(256).strip()
+        print data
+        size, goal, p1, p2 = data.split(',')
+        size, goal = int(size), int(goal)
+        game = Game(size, goal, p1, p2)
+        while not game.winner:
+            print game
+            if game.turn == game.p2:
+                move = raw_input("%s : " % game.turn)
+                connection.sendall(move)
+            else:
+                move = connection.recv(64).strip()
+            game.play_turn(move)
+        print "Player %s wins!" % game.winner
+    finally:
+        connection.close()
 
 
+if __name__ == '__main__':
+    import sys
+    args = sys.argv
+    if '-h' in args:
+        game_host()
+    elif '-c' in args:
+        game_client()
+    else:
+        game_console()
 
 
 
---
+++ scrabble.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# coding=utf-8
+
+def load_words():
+    words = []
+    with open('/usr/share/dict/words') as word_file:
+        for word in word_file:
+            word = word.strip()
+            if word.islower():
+                words.append(word)
+    return words
+
+def is_anagram(word, letters):
+    if len(word) != len(letters):
+        return False
+    word = list(word)
+    for letter in letters:
+        if letter not in word:
+            return False
+        word.remove(letter)
+    return True
+
+
+def find_anagrams1(words, letters):
+    anagrams = []
+    for word in words:
+        if is_anagram(word, letters):
+            anagrams.append(word)
+    return anagrams
+
+def generate_anagram_map(words):
+    letter_map = {}
+    for word in words:
+        key = ''.join(sorted(word))
+        words = letter_map.get(key, [])
+        words.append(word)
+        letter_map[key] = words
+    return letter_map
+
+words = load_words()
+anamap = generate_anagram_map(words)
+print 'here are the first 10 words we loaded:', words[:10]
+
+print 'some anagrams: ', find_anagrams1(words, 'game')
+print 'same stuff', anamap.get('aegm')
