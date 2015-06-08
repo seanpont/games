541d22bb6cf6d457d034c7377deda8d5f71f28f0
Sean Pont <seanpont@gmail.com>
2015-06-07 19:35:38 -0700

    words with friends

---
+++ words.py
@@ -0,0 +1,20 @@
+__author__ = 'tron'
+
+#intro
+
+# how many words start with an a
+print "Welcome a the anagram search dictionary thing"
+
+letter_map = {}
+
+word_file = open('/usr/share/dict/words')
+for line in word_file:
+
+    print line
+
+
+
+
+
+
+
