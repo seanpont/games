6a1d3ef61781df615789b90f522f22084ee066b0
Sean Pont <seanpont@gmail.com>
2016-01-09 21:34:54 -0800

    preparing for the lesson

--- lesson_20160110.py
+++ lesson_20160110.py
@@ -1,17 +1,8 @@
-from test_utils import assert_equal
-
 # WARM-UP
 
 # TODO: Write a function that takes a list of ints and returns only the odd ones
 # Example: odd_filter([1,2,3,4,5]) == [1,3,5]
 # Remember: a number n is odd if n % 2 == 1
-
-
-def odd_filter(xs):
-    return [x for x in xs if x % 2 == 1]
-
-
-assert_equal(odd_filter(range(1, 6)), [1, 3, 5])
 
 
 # TODO: Write a function that can read roman numerals
@@ -36,19 +27,6 @@ SPECIAL_NUMERALS = {
     'CM': 900}
 
 
-def roman_numerals_to_int(numerals):
-    """
-    :type numerals: str
-    """
-    res = sum((SPECIAL_NUMERALS[sn] * numerals.count(sn)
-               for sn in SPECIAL_NUMERALS))
-    numerals = reduce(lambda a, b: a.replace(b, ''), SPECIAL_NUMERALS.keys(), numerals)
-    return res + sum((NUMERALS[n] * numerals.count(n) for n in NUMERALS))
-
-
-assert_equal(14, roman_numerals_to_int('XIV'))
-
-
 # INTRODUCTION TO CLASSES
 
 
@@ -57,29 +35,43 @@ class Person(object):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        self.skills = {}
 
     def name(self):
         return self.first_name + ' ' + self.last_name
 
-    def say_hello(self):
-        print 'Hi, my name is ', self.name(), "and I'm", self.age, 'years old.'
+    def greeting(self):
+        return 'Hi, my name is ', self.name(), "and I'm", self.age, 'years old.'
 
     def age_by(self, years):
         self.age += years
 
+    def practice(self, skill, hours):
+        self.skills[skill] = self.skills.get(skill, 0) + hours
+
     def __str__(self):
         return self.name
+
+    def best_skill(self):
+        return max(self.skills.iteritems(), key=lambda x: x[1])
 
 
 sean = Person("Sean", "Pont", 30)
 cedric = Person("Cedric", "Oltramare", 14)
 alexia = Person("Alexia", "Oltramare", 10)
 
-sean.say_hello()
-cedric.say_hello()
+print sean.greeting()
+print cedric.greeting()
 
 sean.age_by(1)
-sean.say_hello()
+print sean.greeting()
+
+sean.practice('running', 2)
+sean.practice('programming', 8)
+sean.practice('running', 1)
+sean.practice('programming', 8)
+sean.practice('basket weaving', 1)
+print "Sean's best skill:", sean.best_skill()
 
 
 # TODO: create a store class.
@@ -88,8 +80,5 @@ sean.say_hello()
 # Create functions that allow us to stock items, set prices, and buy items.
 # Explore inheritance by creating different types of stores
 # Explore composition by splitting the store up into an inventory, storefront...
-
-
-
 
 
