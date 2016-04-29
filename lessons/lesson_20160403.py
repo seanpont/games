d0a92574ecb5e5c1617dac2ec57668f7b7f896ce
Sean Pont <seanpont@gmail.com>
2016-04-28 21:54:31 -0700

    old lesson

---
+++ lessons/lesson_20160403.py
@@ -0,0 +1,15 @@
+"""
+Websites!
+
+http://chooseyourownadventuremaker.appspot.com
+
+How does it work
+Features we could add:
+  Add color
+  Add a link to the home page when you end a story
+  Create an Account page
+  Add search
+  Add create_at field to story, sort by most recent
+  Add users, points, badges
+
+"""
