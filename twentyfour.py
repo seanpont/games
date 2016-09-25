c46de087a13c1c2aebb542fee00cd4a0c191852c
Sean Pont <seanpont@gmail.com>
2016-09-25 14:43:34 -0700

    fixing bugs in 24

--- twentyfour.py
+++ twentyfour.py
@@ -1,51 +1,31 @@
+from fractions import Fraction
 from random import choice
 
 def add(a, b): return a + b
-
 def sub(a, b): return a - b
+def mul(a, b): return a * b
+def div(a, b): return Fraction(a, b)
+ops = [add, sub, mul, div]
 
-def mult(a, b): return a * b
-
-def div(a, b): return a / float(b)
-
-ops = (add, sub, mult, div)
-
-def solve(*xs):
-  for i in xrange(len(xs)):
-    x, rest = xs[i], xs[:i] + xs[i + 1:]
-    ans = _solve(x, *rest)
-    if ans:
-      return '(((%s %s' % (x, ans)
-  return None
-
-def _solve(val, *xs):
-  if not xs:
-    return ' = 24' if val == 24 else None
-  for i in xrange(len(xs)):
-    x, rest = xs[i], xs[:i] + xs[i + 1:]
-    for op in ops:
-      y = _solve(op(val, x), *rest)
-      if y:
-        return '%s %s) %s' % (op.__name__, x, y)
-
+def solver(target, xs):
+  if len(xs) == 1:
+    return '= 24' if xs[0] == target else None
+  for i in xrange(len(xs) - 1):
+    for j in xrange(i + 1, len(xs)):
+      a, b, rest = xs[i], xs[j], xs[:i] + xs[i + 1:j] + xs[j + 1:]
+      for op in ops:
+        try:
+          ans = solver(target, rest + [op(a, b)])
+        except ZeroDivisionError:
+          continue
+        if ans:
+          return '%s(%s, %s) %s' % (op.__name__, a, b, ans)
 
 if __name__ == '__main__':
-  count = 0
-  for a in range(1, 10):
-    for b in range(1, 10):
-      for c in range(1, 10):
-        for d in range(1, 10):
-          if solve(a, b, c, d):
-            count += 1
-  print 'total combinations: %s of %s' % (count, 9**4)
-
-
-  # while True:
-  #   question = [choice(range(1, 10)) for _ in range(4)]
-  #   answer = solve(*question)
-  #   if answer:
-  #     print '\n', question
-  #     raw_input('')
-  #     print answer
-
-
+  while True:
+    question = [choice(range(1, 10)) for _ in range(4)]
+    answer = solver(24, question)
+    if answer:
+      print '\n', question
+      raw_input('')
+      print answer
